da9db04a2250b9868af06ab75b6755fba8cd5189
eduardosao <eduardo.sao@gmail.com>
2021-08-30 18:51:56 -0300

    Primeiro

---
+++ Sistema.js
@@ -0,0 +1,18 @@
+// Eduardo T Santos
+const Qtd_Alunos = 15;
+
+    for(let i = 0; i <= Qtd_Alunos; i = i + 1 ) 
+        {
+            if (i === 0 )
+                {
+                    console.log("O numero atual é zero")
+                }
+            else if (i % 2 == 0)
+                {
+                    console.log("O numero atual " + i + " é Par")
+                }
+            else
+                 {
+                    console.log("O numero atual " + i + " é Impar")
+                }    
+        }
